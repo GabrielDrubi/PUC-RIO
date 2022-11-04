901a77147426c93018a5f64b4be1c25563b42b54
Gabriel Drubi <100882137+GabrielDrubi@users.noreply.github.com>
2022-11-04 17:19:09 -0300

    Add files via upload

---
+++ NFLML/explore_page.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+def show_explore_page():
+    st.title("Explore Software Engineer Salaries")
+
+    st.write(
+        """
+    ### Stack Overflow Developer Survey 2020
+    """
+    )
